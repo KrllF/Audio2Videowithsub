8f0bf1db799d62917f739bf78d5eaff24f3fb4cd
Kirill Filichkin <112120031+KrllF@users.noreply.github.com>
2024-09-11 00:36:58 +0300

    Update config.py

--- telegram-bot/config.py
+++ telegram-bot/config.py
@@ -1,2 +1,3 @@
 TOKEN = "7544063519:AAEu8fsQDzcwunlOjhUxvOooyWEX5TRSyag"
-background_path = "total_black_background.jpg"
+background_path_audio = "total_black_background.jpg"
+background_path_video = "fix_audio/video_file_normalized/video_file_plus_output.mp4"
