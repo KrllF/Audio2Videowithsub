5ce720f556f92648df8819f5944ef6f2490f8cc8
Kirill Filichkin <112120031+KrllF@users.noreply.github.com>
2024-09-10 19:42:50 +0300

    создание видео с вокалом и без

--- func_file.py
+++ func_file.py
@@ -6,6 +6,8 @@ import subprocess
 import ffmpeg
 import os
 
+from config import background_path
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model_whisper = whisper.load_model("small", device=device)
@@ -46,7 +48,7 @@ def audio_split(input_file, output_folder):
         print(f"Стандартный вывод: {e.stdout}")
         print(f"Стандартный поток ошибок: {e.stderr}")
     except Exception as e:
-        print(f"Возникла непредвиденная ошибка: {e}")
+        print(f"Возникла ошибка: {e}")
 
 def subtitles(vocals_path, audio_name):
 
@@ -89,16 +91,17 @@ def ffmpeg_command(audio_name, background_path, audio_path, subtitles_path, outp
     ]
 
 def rndr_video(audio_name):
-    background_path = "total_black_background.jpg"  # Измените на другое изображение по желанию
-    audio_path = f"fix_audio/{audio_name}_normalized/accompaniment.wav"
+    audio_path_minus = f"fix_audio/{audio_name}_normalized/accompaniment.wav"
+    audio_path_plus = f"fix_audio/{audio_name}_normalized.wav"
     subtitles_path = f"fix_audio/{audio_name}_normalized/subtitles.srt"
-    output_path = f"fix_audio/{audio_name}_normalized/{audio_name}_output.mp4"
-
-    command = ffmpeg_command(audio_name, background_path, audio_path, subtitles_path, output_path)
+    output_path_minus = f"fix_audio/{audio_name}_normalized/{audio_name}_minus_output.mp4"
+    output_path_plus = f"fix_audio/{audio_name}_normalized/{audio_name}_plus_output.mp4"
+    command_minus = ffmpeg_command(audio_name, background_path, audio_path_minus, subtitles_path, output_path_minus)
+    command_plus = ffmpeg_command(audio_name, background_path, audio_path_plus, subtitles_path, output_path_plus)
 
     try:
-        subprocess.run(command, check=True)
-        print("Видео успешно создано.")
+        subprocess.run(command_minus, check=True)
+        subprocess.run(command_plus, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Ошибка при выполнении команды: {e}")
 
@@ -110,13 +113,15 @@ def output_video(audio_name, audio_format):
 
     audio_split(normalized_audio_name, "fix_audio")
 
+
+    subtitles(f"fix_audio/{audio_name}_normalized/vocals.wav", audio_name=audio_name)
+    rndr_video(audio_name)
+
     if os.path.exists(normalized_audio_name):
         os.remove(normalized_audio_name)
         print(f"Deleted file: {normalized_audio_name}")
     else:
         print(f"File not found: {normalized_audio_name}")
 
-    subtitles(f"fix_audio/{audio_name}_normalized/vocals.wav", audio_name=audio_name)
-    rndr_video(audio_name)
 
 
